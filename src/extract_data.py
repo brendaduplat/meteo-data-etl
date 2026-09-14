cede366715bba7170b2afe8a815b78406c8a5741
Brenda Duplat <brendaduplat352@gmail.com>
2026-09-14 19:04:19 -0300

    refactor: adjust extract_data.py

--- src/extract_data.py
+++ src/extract_data.py
@@ -5,14 +5,14 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def extract_weather_data(url: str) -> list | None:
+def extract_weather_data(url: str) -> list:
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
     except requests.exceptions.RequestException as erro:
         logging.error(f"Erro {erro} na requisição")
-        return None
+        raise
 
     output_path = "data/weather_data.json"
     output_dir = Path(output_path).parent
